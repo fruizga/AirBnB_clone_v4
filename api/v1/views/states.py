0aa75f39ac514cf8760eff8f6b11036c90b7c777
fruizga <ruizgallego@gmail.com>
2020-05-18 22:17:46 -0500

    prev

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
+from flasgger import Swagger, swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
