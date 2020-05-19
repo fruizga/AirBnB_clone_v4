dd86c46462ac4bb2bf23300c040970860e4c811c
juliancastaeda <1277@holbertonschool.com>
2020-05-19 10:11:58 -0500

    upload file 0

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,13 +7,11 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
+from uuid import uuid4
 
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
-
-cache_id = uuid.uuid4()
 
 @app.teardown_appcontext
 def close_db(error):
@@ -40,7 +38,7 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=cache_id)
+                           places=places, cache_id=uuid4())
 
 
 if __name__ == "__main__":
